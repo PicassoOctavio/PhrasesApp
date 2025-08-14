6d28f28d7eb0c50b5ba4bc10d561603b94ceb401
PicassoOctavio <54865779+PicassoOctavio@users.noreply.github.com>
2025-08-14 13:33:24 -0300

    Update CardsGrid.tsx

--- phrases-app/src/components/CardsGrid/CardsGrid.tsx
+++ phrases-app/src/components/CardsGrid/CardsGrid.tsx
@@ -1,14 +1,16 @@
+import { useSelector } from "react-redux";
 import { Box } from "@mui/material";
 import { Card } from "../Card/Card";
-import { mockedData } from "../../data/mockedData";
+import type { PhraseI } from "../../interfaces/phrasesI";
 
 import "./CardsGrid.css";
 
 export const CardsGrid = () => {
+  const { phrases } = useSelector((state) => state.phrases);
   return (
     <Box className="cards-grid">
-      {mockedData.map((item) => (
-        <Card key={item.id} id={item.id} description={item.description} />
+      {phrases.map((phrase: PhraseI) => (
+        <Card key={phrase.id} id={phrase.id} description={phrase.description} />
       ))}
     </Box>
   );
