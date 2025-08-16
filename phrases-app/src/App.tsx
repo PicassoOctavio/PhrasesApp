ed004b5c113f1fe9d8ea81d299b4b770bea6c6d9
PicassoOctavio <54865779+PicassoOctavio@users.noreply.github.com>
2025-08-16 14:26:27 -0300

    Update App.tsx

--- phrases-app/src/App.tsx
+++ phrases-app/src/App.tsx
@@ -1,9 +1,10 @@
+import { Container } from "@mui/material";
 import { AppRouter } from "./router/Router";
 
 export const App = () => {
   return (
-    <>
+    <Container className="app-contianer">
       <AppRouter />
-    </>
+    </Container>
   );
 };
