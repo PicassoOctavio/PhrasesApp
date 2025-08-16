1ee15e540b1a9923d92987abdf0e4f7fa22ff7ca
PicassoOctavio <54865779+PicassoOctavio@users.noreply.github.com>
2025-08-16 14:32:49 -0300

    Update Card.tsx

--- phrases-app/src/components/Card/Card.tsx
+++ phrases-app/src/components/Card/Card.tsx
@@ -1,5 +1,5 @@
 import { useDispatch } from "react-redux";
-import { IconButton, Typography } from "@mui/material";
+import { IconButton, Typography, useTheme } from "@mui/material";
 import { Close } from "@mui/icons-material";
 import { motion, useMotionValue, useTransform } from "framer-motion";
 import { deletePhrase } from "../../store/phrasesSlice";
@@ -16,6 +16,7 @@ export const Card = ({ id, description }: CardProps) => {
   const y = useMotionValue(0);
   const rotateX = useTransform(y, [-100, 100], [60, -60]);
   const rotateY = useTransform(x, [-100, 100], [-60, 60]);
+  const theme = useTheme();
 
   const dispatch = useDispatch();
 
@@ -31,6 +32,7 @@ export const Card = ({ id, description }: CardProps) => {
         y,
         rotateX,
         rotateY,
+        backgroundColor: theme.palette.background.paper,
       }}
       drag
       dragConstraints={{ top: 0, right: 0, bottom: 0, left: 0 }}
@@ -40,6 +42,7 @@ export const Card = ({ id, description }: CardProps) => {
         scale: 1.05,
         boxShadow: "0px 15px 30px rgba(0,0,0,0.3)",
         zIndex: 2,
+        backgroundColor: "white",
       }}
       transition={{ type: "spring", stiffness: 300, damping: 20 }}
     >
@@ -49,8 +52,8 @@ export const Card = ({ id, description }: CardProps) => {
           top: 8,
           right: 8,
           color: "black",
-          width: 24,
-          height: 24,
+          width: 18,
+          height: 18,
           "&:hover": { bgcolor: "white", borderRadius: "50%" },
         }}
         onClick={handleDelete}
