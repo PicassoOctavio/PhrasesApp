c53c53ebe329e748fc0cb36feac3a7a64399b076
PicassoOctavio <54865779+PicassoOctavio@users.noreply.github.com>
2025-08-16 14:34:34 -0300

    Update main.tsx

--- phrases-app/src/main.tsx
+++ phrases-app/src/main.tsx
@@ -5,13 +5,17 @@ import { App } from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./theme/appTheme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
     </Provider>
   </StrictMode>
 );
