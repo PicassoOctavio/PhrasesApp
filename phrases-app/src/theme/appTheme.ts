fe34270c40f9a4f47a0db62173f3471dc142c0f9
PicassoOctavio <54865779+PicassoOctavio@users.noreply.github.com>
2025-08-18 11:23:50 -0300

    Update appTheme.ts

--- phrases-app/src/theme/appTheme.ts
+++ phrases-app/src/theme/appTheme.ts
@@ -20,39 +20,7 @@ const theme = createTheme({
     },
     text: {
       primary: "#000000ff",
-      secondary: "#555555",
-    },
-  },
-  components: {
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          borderRadius: 12,
-          transition: "background-color 0.3s ease",
-        },
-        containedPrimary: {
-          "&:hover": {
-            backgroundColor: "#8ca8db", // Un poco más oscuro para hover
-          },
-        },
-        containedSecondary: {
-          "&:hover": {
-            backgroundColor: "#eaaed8", // Rosa más intenso en hover
-          },
-        },
-      },
-    },
-    MuiPaper: {
-      styleOverrides: {
-        root: {
-          transition: "transform 0.3s ease, box-shadow 0.3s ease",
-          "&:hover": {
-            transform: "translateY(-4px)",
-            boxShadow: "0px 8px 20px rgba(0,0,0,0.15)",
-            backgroundColor: "rgba(255,255,255,0.9)",
-          },
-        },
-      },
+      secondary: "#ffffffff",
     },
   },
 });
