143c9f6db09a3b67668a380f812505b1e74d221f
PicassoOctavio <54865779+PicassoOctavio@users.noreply.github.com>
2025-08-18 11:46:53 -0300

    Update App.tsx

--- phrases-app/src/App.tsx
+++ phrases-app/src/App.tsx
@@ -1,10 +1,13 @@
 import { Container } from "@mui/material";
 import { AppRouter } from "./router/Router";
+import { Footer } from "./components/Footer/Footer";
 
 export const App = () => {
   return (
     <Container className="app-contianer">
       <AppRouter />
+
+      <Footer />
     </Container>
   );
 };
