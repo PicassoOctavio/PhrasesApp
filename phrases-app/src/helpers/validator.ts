319a55e3029fbc4a62c9d54bdd60ee9e35574864
PicassoOctavio <54865779+PicassoOctavio@users.noreply.github.com>
2025-09-01 18:25:46 -0300

    Update validator.ts

--- phrases-app/src/helpers/validator.ts
+++ phrases-app/src/helpers/validator.ts
@@ -1,10 +1,6 @@
 export const validatePhrase = (text: string) => {
-  const auxText = text.trim();
-  if (auxText) {
-    return text.trim().length <= 150;
-  } else {
-    return false;
-  }
+  const auxText = text.trim().replace(/\s{2,}/g, " ");
+  return auxText.length >= 5 && auxText.length <= 200;
 };
 
 export const escapeRegExp = (s: string) =>
