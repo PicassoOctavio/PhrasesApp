90c72719a4560a09b7479a845e742f4c1de2e2f6
PicassoOctavio <54865779+PicassoOctavio@users.noreply.github.com>
2025-09-03 16:45:16 -0300

    Update vite.config.ts

--- phrases-app/vite.config.ts
+++ phrases-app/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: "jsdom", // 👈 necesario
+    globals: true, // describe/it/expect globales
+    setupFiles: "./vitest.setup.ts", // 👈 tu setup con jest-dom
+  },
+});
